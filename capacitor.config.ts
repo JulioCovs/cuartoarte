985a58f5b8258aa826c783e0b98870c994688605
JulioCovs <julioccesarcov@gmail.com>
2026-04-29 14:29:30 -0600

    add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cuartoarte.app',
+  appName: 'CuartoArte',
+  server: {
+    url: 'https://TU-URL-DE-RAILWAY',
+    cleartext: true
+  }
+};
+
+export default config;
